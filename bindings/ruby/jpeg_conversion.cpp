cb50819e0ab08e7b5a0e19a004530e15f2eeff92
Thomas Roehr <thomas.roehr@dfki.de>
2012-09-20 19:04:42 +0200

    Fix loading jpeg via ruby

--- bindings/ruby/jpeg_conversion.cpp
+++ bindings/ruby/jpeg_conversion.cpp
@@ -11,6 +11,31 @@
 using namespace Rice;
 
 Rice::Data_Type<conversion::JpegConversion> rb_cJpegConversion;
+typedef std::vector<uint8_t> ImageBuffer;
+
+template<>
+Object to_ruby< ImageBuffer >(const ImageBuffer& data)
+{
+    Array array;
+    ImageBuffer::const_iterator it = data.begin();
+    for(; it != data.end(); ++it)
+    {
+        array.push(*it);
+    }
+    return array;
+}
+
+Array do_from_file(Object self, String filename, int width, int height)
+{
+    Data_Object<conversion::JpegConversion> conversion(self, rb_cJpegConversion);
+    std::string name = filename.str();
+
+    //Data_Object<base::samples::frame::Frame> frame(new base::samples::frame::Frame(width, height));
+    base::samples::frame::Frame frame;
+    conversion->loadJpeg(name, width, height, frame);
+    // std::vector<uint8_t>
+    return to_ruby< ImageBuffer >(frame.image);
+}
 
 Object do_store_frame(Object self, String filename, Object roptr_frame)
 {
@@ -35,7 +60,7 @@ void Init_jpeg_conversion_ext()
      rb_cJpegConversion = define_class_under<conversion::JpegConversion>(rb_mJpegConversion, "JpegConversion")
    .define_constructor(Constructor<conversion::JpegConversion, unsigned int>(), Arg("jpeg quality"))
    .define_constructor(Constructor<conversion::JpegConversion>())
-   .define_method("load_jpeg", &conversion::JpegConversion::loadJpeg, (Arg("filename"), Arg("width"), Arg("height"), Arg("frame")))
+   .define_method("do_from_file", &do_from_file, (Arg("filename"), Arg("width"), Arg("height")))
    .define_method("store_frame", &do_store_frame, (Arg("filename"), Arg("frame")))
    ;
 }
--- bindings/ruby/jpeg_conversion.rb
+++ bindings/ruby/jpeg_conversion.rb
@@ -1,1 +1,18 @@
 require 'jpeg_conversion_ext'
+
+module Conversion
+    class JpegConversion
+        def from_file(file, width, height)
+            frame = Types::Base::Samples::Frame::Frame.new
+            frame.size.height = height
+            frame.size.width = width
+            frame.frame_mode = :MODE_JPEG
+            frame.image = do_load_jpeg(file,width,height)
+            return frame
+        end
+
+        def load_jpeg(*args)
+            raise RuntimeError, "load_jpeg is not supported any more. Use from_file(filename, image_width, image_height)"
+        end
+    end
+end
